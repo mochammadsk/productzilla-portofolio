a869e5ce47414982d393067b513d6e6b087bd587
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-19 22:50:58 +0800

    initiation home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,18 @@
+function Home() {
+  return (
+    <div>
+      <header>
+        <ul className='list-none'>
+          <li>Home</li>
+          <li>Experience</li>
+          <li>Project</li>
+        </ul>
+      </header>
+      <main>
+        <h1>Mochammad Syahrul Kurniawan</h1>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
