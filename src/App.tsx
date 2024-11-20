dfd26c9e9df01688157e69a53f1510729ab2612f
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-20 15:15:41 +0800

    add router experience and project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Overview from './pages/Overview';
+import Experience from './pages/Experience';
+import Project from './pages/Project';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/Home' element={<Home />} />
+        <Route path='/overview' element={<Overview />} />
+        <Route path='/experience' element={<Experience />} />
+        <Route path='/project' element={<Project />} />
       </Routes>
     </Router>
   );
