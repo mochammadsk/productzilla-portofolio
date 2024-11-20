acf13a987e612e75eaf56df6d58bd822d691603c
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-21 04:22:23 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Overview from './pages/Overview';
 import Experience from './pages/Experience';
 import Project from './pages/Project';
