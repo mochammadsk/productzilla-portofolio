0b7fb00af7c64335c1bd0185a0c94eb734d3f75a
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-20 16:40:58 +0800

    add basename router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Overview from './pages/Overview';
 import Experience from './pages/Experience';
 import Project from './pages/Project';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename='/overview'>
       <Routes>
         <Route path='/overview' element={<Overview />} />
         <Route path='/experience' element={<Experience />} />
         <Route path='/project' element={<Project />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
