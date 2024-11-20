f5ffbc22ea402952bcfc4a4cb60461847926fda6
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-20 15:17:20 +0800

    initiation pages experience

---
+++ src/pages/Experience/index.tsx
@@ -0,0 +1,12 @@
+import Header from '../../components/Header';
+
+function Experience() {
+  return (
+    <>
+      <Header />
+      <h1>Experience</h1>
+    </>
+  );
+}
+
+export default Experience;
